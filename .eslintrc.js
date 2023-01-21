a0cde0871023ad018486215959db2fbbfe9ffd72
BRAINIAC-DESKTO\janwe <1134888+BrainiacSmiley@users.noreply.github.com>
2023-01-21 07:15:19 +0100

    - added appTitle to env File - reenabled eslint

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-strongly-recommended",
+    "plugin:prettier/recommended",
+    "plugin:@intlify/vue-i18n/recommended",
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2022,
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "@intlify/vue-i18n/no-dynamic-keys": "error",
+    "@intlify/vue-i18n/no-unused-keys": [
+      "error",
+      {
+        extensions: [".js", ".vue"],
+      },
+    ],
+  },
+  settings: {
+    "vue-i18n": {
+      localeDir: "@locales/*.{json,json5,yaml,yml}", // extension is glob formatting!
+
+      // Specify the version of `vue-locales` you are using.
+      // If not specified, the message will be parsed twice.
+      messageSyntaxVersion: "^9.2.2",
+    },
+  },
+};
